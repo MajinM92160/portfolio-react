5ca6b3d72d66ed4f2b2ad03b75f340b3706ab6ca
MajinM92160 <mkacem6@outlook.com>
2023-01-05 13:06:14 +0100

    adding for my portfolio all assets and the data for home page and my all social media

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/ashitaka808/" className="home__social-icon" target="_blank">
+                <i class="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://twitter.com/Jubei92" className="home__social-icon" target="_blank">
+                <i className="uil uil-twitter"></i>
+            </a>
+
+            <a href="https://github.com/MajinM92160" className="home__social-icon" target="_blank">
+                <i className="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social;
+
