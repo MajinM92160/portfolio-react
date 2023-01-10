7b7b6a957a56d6ebcbc2f22e1db204efee707171
MajinM92160 <mkacem6@outlook.com>
2023-01-10 17:12:44 +0100

    added testimonials components

---
+++ src/components/testimonial/Testimonials.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Testimonials(props) {
+    return (
+        <section className="testimonial container section">
+            <h2 className="section__title">My </h2>
+        </section>
+    );
+}
+
+export default Testimonials;
